27c4be0e0c29c2e769feebdfd27825a367ccff9b
LowkeyLoki75 <braydenqtran@gmail.com>
2026-02-01 18:38:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -71,8 +71,7 @@ const CONFIG = {
         startText: "🎵 Play Music",       
         stopText: "🔇 Stop Music",        
         volume: 0.5   
-    }
-                                       
+    }                                      
 };
 
 // Don't modify anything below this line unless you know what you're doing
